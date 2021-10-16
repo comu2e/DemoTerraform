db17bdac95e97ea93c087ef10cc712712921390a
comu2e <comu2e@gmail.com>
2021-10-17 03:15:27 +0900

    instance設定

--- main.tf
+++ main.tf
@@ -4,3 +4,206 @@ provider "aws" {
 terraform {
   required_version = "~> 1.0.8"
 }
+
+variable "app_name" {
+  type    = string
+  default = "FargateDemo"
+}
+variable "azs" {
+  type    = list(string)
+  default = ["ap-northeast-1a", "ap-northeast-1c"]
+}
+variable "vpc_cidr" {
+  type    = string
+  default = "10.10.0.0/16"
+}
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.10.0.0/24", "10.10.1.0/24"]
+}
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.10.10.0/24", "10.10.11.0/24"]
+}
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    "Name" = "${var.app_name}"
+  }
+}
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidrs[count.index]
+  availability_zone = var.azs[count.index]
+  tags = {
+    "Name" = "${var.app_name}-Public-${count.index}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.azs[count.index]
+  tags = {
+    "Name" = "${var.app_name}-Private-${count.index}"
+  }
+}
+
+
+# IGW
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = var.app_name
+  }
+}
+
+# RouteTable(Public)
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.app_name}-public"
+  }
+}
+
+# Route(Public)
+resource "aws_route" "public" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+# RouteTableAssociation(Public)
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidrs)
+
+  subnet_id      = element(aws_subnet.publics.*.id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
+# EC2
+resource "aws_instance" "main" {
+  ami           = "ami-0f27d081df46f326c"
+  instance_type = "t3.nano"
+  key_name      = aws_key_pair.main.id
+
+  vpc_security_group_ids = [aws_security_group.main.id]
+  subnet_id              = var.subnet_id
+
+  # EBS最適化
+  ebs_optimized = true
+
+  # EBS
+  root_block_device {
+    volume_size           = 8
+    volume_type           = "gp3"
+    iops                  = 3000
+    throughput            = 125
+    delete_on_termination = true
+
+    tags = {
+      Name = var.app_name
+    }
+  }
+
+  tags = {
+    Name = var.app_name
+  }
+}
+
+# SecurityGroup
+resource "aws_security_group" "main" {
+  vpc_id = var.vpc_id
+
+  name        = "${var.app_name}-ec2"
+  description = "${var.app_name}-ec2"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.app_name}-ec2"
+  }
+}
+
+# SecurityGroupRule
+resource "aws_security_group_rule" "ssh" {
+  security_group_id = aws_security_group.main.id
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+}
+
+resource "aws_security_group_rule" "http" {
+  security_group_id = aws_security_group.main.id
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+}
+
+# SSHKey
+# ご自身でデモする場合はssh-keygenでrsaキーを作成してpublic keyに設定してください
+resource "aws_key_pair" "main" {
+  key_name   = "sample-ec2-key"
+  public_key = file("./ec2/ec2.pub")
+}
+# EIP
+resource "aws_eip" "main" {
+  instance = aws_instance.main.id
+  vpc      = true
+
+  tags = {
+    Name = var.app_name
+  }
+}
+# SecurityGroup
+resource "aws_security_group" "main" {
+  vpc_id = var.vpc_id
+
+  name        = "${var.app_name}-ec2"
+  description = "${var.app_name}-ec2"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.app_name}-ec2"
+  }
+}
+
+# SecurityGroupRule
+resource "aws_security_group_rule" "ssh" {
+  security_group_id = aws_security_group.main.id
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+}
+
+resource "aws_security_group_rule" "http" {
+  security_group_id = aws_security_group.main.id
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+}
