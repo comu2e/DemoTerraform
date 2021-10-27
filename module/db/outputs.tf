b00c2861eedc63a8012ac2009c5bad94831180de
comu2e <comu2e@gmail.com>
2021-10-27 19:49:36 +0900

    2serviceに対応

--- module/db/outputs.tf
+++ module/db/outputs.tf
@@ -1,3 +1,9 @@
 output "endpoint" {
   value = aws_db_instance.main.endpoint
 }
+output "db_private_subnet" {
+  value = aws_db_instance.main.db_subnet_group_name
+}
+output "db_sg" {
+  value = aws_security_group.main.id
+}
--- module/ecs/main.tf
+++ module/ecs/main.tf
@@ -1,47 +1,6 @@
-variable "app_name" {
-  type = string
-}
-variable "cluster" {
-  description = "ECS Cluster"
-  type        = string
-}
-variable "aws_iam_role_task_exection_arn" {
-  description = "ECS Task execution IAM role arn"
-  type        = string
-}
-# public subnetに配置するか、private subnetに配置するかを制御する
-variable "placement_subnet" {
-  description = "ECS placement subnet.Public subnet or Private subnet is expected."
-  type        = list(string)
-}
-
-variable "sg" {
-  description = "ECS security group.HTTP/HTTP security group is expected"
-  type        = list(string)
-}
-
-variable "target_group_arn" {
-  type = string
-}
-data "aws_region" "current" {}
-data "aws_caller_identity" "current" {}
-locals {
-  account_id = data.aws_caller_identity.current.account_id
-  region     = data.aws_region.current.name
-}
-data "template_file" "container_definitions" {
-  template = file(abspath("./module/ecs/container_definitions.json"))
-  # templateのjsonファイルに値を渡す
-  vars = {
-    tag        = "latest"
-    name       = var.app_name
-    account_id = local.account_id
-    region     = local.region
-  }
-}
 # タスク定義
 resource "aws_ecs_task_definition" "main" {
-  family = var.app_name
+  family = "${var.app_name}-${var.entry_container_name}"
 
   cpu                      = 256
   memory                   = 512
@@ -61,7 +20,7 @@ resource "aws_ecs_task_definition" "main" {
 resource "aws_ecs_service" "main" {
   #   depends_on = [aws_lb_listener_rule.main]
 
-  name = var.app_name
+  name = "${var.app_name}-${var.entry_container_name}"
 
   launch_type      = "FARGATE"
   platform_version = "1.4.0"
@@ -83,12 +42,12 @@ resource "aws_ecs_service" "main" {
 
   load_balancer {
     target_group_arn = var.target_group_arn
-    container_name   = "nginx"
-    container_port   = 80
+    container_name   = var.entry_container_name
+    container_port   = var.entry_container_port
   }
 }
 # Log
 resource "aws_cloudwatch_log_group" "main" {
-  name              = "/${var.app_name}/ecs"
+  name              = "/${var.app_name}-${var.entry_container_name}/ecs"
   retention_in_days = 7
 }
