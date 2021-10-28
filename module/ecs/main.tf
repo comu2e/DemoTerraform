8fdae401f8417cc12ee28d0c875d79006ac75bf5
comu2e <comu2e@gmail.com>
2021-10-28 12:53:58 +0900

    正常に動作

--- module/ecs/main.tf
+++ module/ecs/main.tf
@@ -48,6 +48,6 @@ resource "aws_ecs_service" "main" {
 }
 # Log
 resource "aws_cloudwatch_log_group" "main" {
-  name              = "/${var.app_name}-${var.entry_container_name}/ecs"
+  name              = "/${var.app_name}/ecs"
   retention_in_days = 7
 }
