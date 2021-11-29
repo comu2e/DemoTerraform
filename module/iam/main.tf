3a375f5be466b10ef5c85873de6b2a18f0889c21
comu2e <comu2e@gmail.com>
2021-11-29 22:28:15 +0900

    動作するバージョンに修正

--- module/iam/main.tf
+++ module/iam/main.tf
@@ -1,3 +1,4 @@
+
 variable "app_name" {
   type = string
 }
--- src/dev/variables.tf
+++ src/dev/variables.tf
@@ -1,6 +1,6 @@
 variable "app_name" {
   type    = string
-  default = "app-dev"
+  default = "app"
 }
 variable "azs" {
   type    = list(string)
