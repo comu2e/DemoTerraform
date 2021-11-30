f44c8e82de02f722710b78a971f248915b3b2d3f
comu2e <comu2e@gmail.com>
2021-12-01 01:22:43 +0900

    comment追加

--- aws.sh
+++ aws.sh
@@ -1,15 +1,19 @@
-# cat $ENV_file | #!/bin/bash
+#usage
+# cp .env.examp .env.dev
+# input env value .env.dev
+# sh aws.sh .env.dev app_name
+
 APP_NAME=$2
-# 行数カウンタを初期化
+# initialize raw count
 line_no=1
 
-# read コマンドで読み取れなくなるまでループ
 while read LINE
 do
   KEY=${LINE%%=*}
   VALUE=${LINE#*=}
-
+  #if $VALUE is not set or not filled.
   if [ -n "$VALUE" ]; then
+  #execute aws cli command.
   echo "excuted aws ssm put-parameter --type SecureString --name "/$APP_NAME/$KEY" --value "$VALUE"  --overwrite"
   aws ssm put-parameter --type SecureString --name "/$APP_NAME/$KEY" --value "$VALUE"  --overwrite 
   fi
