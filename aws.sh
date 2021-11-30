4b514d411ab9374dec2bad075b530db88e0e3653
comu2e <comu2e@gmail.com>
2021-12-01 01:14:55 +0900

    .env設定sh追加

--- aws.sh
+++ aws.sh
@@ -1,2 +1,21 @@
+# cat $ENV_file | #!/bin/bash
+APP_NAME=$2
+# 行数カウンタを初期化
+line_no=1
 
-cat $ENV_file | aws ssm put-parameter --type SecureString --name "/${app_nameを入力}/該当するキー" --value "該当する値"  --overwrite 
+# read コマンドで読み取れなくなるまでループ
+while read LINE
+do
+  KEY=${LINE%%=*}
+  VALUE=${LINE#*=}
+
+  if [ -n "$VALUE" ]; then
+  echo "excuted aws ssm put-parameter --type SecureString --name "/$APP_NAME/$KEY" --value "$VALUE"  --overwrite"
+  aws ssm put-parameter --type SecureString --name "/$APP_NAME/$KEY" --value "$VALUE"  --overwrite 
+  fi
+
+  line_no=`expr $line_no + 1`
+
+done <$1
+
+exit 0
