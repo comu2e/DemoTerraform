1ec39a30aafc2d84506fdde8d18bd7ae11c7c557
comu2e <comu2e@gmail.com>
2021-12-05 21:47:34 +0900

    Readmeの修正,shellの修正

--- diff.sh
+++ diff.sh
@@ -1,4 +1,12 @@
-#! bin/bash
-colordiff src/$1/main.tf src/$2/main.tf
-colordiff src/$1/variables.tf src/$2/variables.tf
-colordiff src/$1/output.tf src/$2/output.tf     
+#! /bin/bash
+set -euC
+
+REF=$1
+TARGET=$2
+echo $REF
+SCOPE_REF=src/${REF}
+SCOPE_TARGET=src/${TARGET}
+
+colordiff ${SCOPE_REF}/main.tf ${SCOPE_TARGET}/main.tf
+colordiff ${SCOPE_REF}/variables.tf ${SCOPE_TARGET}/variables.tf
+colordiff ${SCOPE_REF}/output.tf ${SCOPE_TARGET}/output.tf     
